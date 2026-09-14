29ccef3c3fe7e77103ef2a55ac5fb83cd5766534
Roberto Justino <robbertojustinno@gmail.com>
2026-09-14 11:18:29 -0300

    chore: aponta admin fase 2 para viewer homologacao

--- admin/config.js
+++ admin/config.js
@@ -2,7 +2,7 @@ window.TAGCHECK_ADMIN_CONFIG = {
   APP_VERSION: '3.0.0',
   APP_NAME: 'TagCheck Admin',
   API_BASE_URL: 'https://tagcheck-fase2-hml-api.onrender.com',
-  VIEWER_BASE_URL: 'https://tag-viewer.onrender.com/',
+  VIEWER_BASE_URL: 'https://tagcheck-fase2-hml-viewer.onrender.com/',
   REQUEST_TIMEOUT_MS: 15000,
   STORAGE_KEYS: {
     language: 'tagcheck_admin_language',
